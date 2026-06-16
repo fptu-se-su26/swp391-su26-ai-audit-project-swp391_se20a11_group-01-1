f951c6add951c389a9a88abf9974abaae91bc5d6
thuong1703n <thuong1703n@gmail.com>
2026-06-16 07:45:54 +0700

    Delete srccode/backend directory

--- srccode/backend/restaurant_management_system/src/main/java/com/rms/restaurant_management_system/service/interfaces/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.rms.restaurant_management_system.service.interfaces;
-
-public interface EmailService {
-
-    void sendOtpEmail(String toEmail, String otp);
-}
--- srccode/backend/restaurant_management_system/src/main/java/com/rms/restaurant_management_system/service/interfaces/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.rms.restaurant_management_system.service.interfaces;
-
-import com.rms.restaurant_management_system.dto.response.UserResponse;
-
-public interface UserService {
-
-    UserResponse getProfileByEmail(String email);
-}
