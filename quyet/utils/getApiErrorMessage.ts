fd7478dcf23e2fb6b4fb62a9a34b81044b94da78
quyetpham2k5 <phamvanquyet5147@gmail.com>
2026-07-02 09:12:20 +0700

    Delete quyet directory

--- quyet/utils/getApiErrorMessage.ts
+++
@@ -1,4 +0,0 @@
-export const getApiErrorMessage = (err: unknown, defaultMessage: string = 'Lỗi hệ thống. Vui lòng thử lại sau.'): string => {
-  const errorObj = err as { response?: { data?: { message?: string } } };
-  return errorObj?.response?.data?.message || defaultMessage;
-};
