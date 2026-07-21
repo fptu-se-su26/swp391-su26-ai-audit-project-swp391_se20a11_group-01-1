fd87f25011b94a49190d6bea0932ede0d48992db
quyetpham2k5 <phamvanquyet5147@gmail.com>
2026-07-21 22:38:52 +0700

    fix(db): make backend hardening migration executable

--- db_migration_backend_security_coordination.sql
+++ db_migration_backend_security_coordination.sql
@@ -1,5 +1,9 @@
 /* UC-01/04/05/06 + reservation/table/payment hardening (SQL Server, rerunnable). */
 
+SET XACT_ABORT ON;
+BEGIN TRY
+BEGIN TRANSACTION;
+
 IF OBJECT_ID('password_reset_tokens', 'U') IS NULL
 BEGIN
     CREATE TABLE password_reset_tokens (
@@ -19,23 +23,33 @@ IF COL_LENGTH('reservations', 'start_at') IS NULL ALTER TABLE reservations ADD s
 IF COL_LENGTH('reservations', 'end_at') IS NULL ALTER TABLE reservations ADD end_at DATETIME2 NULL;
 IF COL_LENGTH('reservations', 'created_order_id') IS NULL ALTER TABLE reservations ADD created_order_id BIGINT NULL;
 
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'ux_reservations_created_order_id')
-    CREATE UNIQUE FILTERED INDEX ux_reservations_created_order_id ON reservations(created_order_id)
-    WHERE created_order_id IS NOT NULL;
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'ux_reservations_created_order_id'
+               AND object_id = OBJECT_ID('reservations'))
+    EXEC('CREATE UNIQUE INDEX ux_reservations_created_order_id ON reservations(created_order_id)
+          WHERE created_order_id IS NOT NULL');
 
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'ix_reservations_table_slot')
-    CREATE INDEX ix_reservations_table_slot ON reservations(assigned_table_id, start_at, end_at, status);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'ix_reservations_table_slot'
+               AND object_id = OBJECT_ID('reservations'))
+    EXEC('CREATE INDEX ix_reservations_table_slot
+          ON reservations(assigned_table_id, start_at, end_at, status)');
 
 IF COL_LENGTH('restaurant_tables', 'original_capacity') IS NULL
 BEGIN
     ALTER TABLE restaurant_tables ADD original_capacity INT NULL;
-    UPDATE restaurant_tables SET original_capacity = capacity WHERE original_capacity IS NULL;
-    ALTER TABLE restaurant_tables ALTER COLUMN original_capacity INT NOT NULL;
+    EXEC('UPDATE restaurant_tables SET original_capacity = capacity WHERE original_capacity IS NULL');
+    EXEC('ALTER TABLE restaurant_tables ALTER COLUMN original_capacity INT NOT NULL');
 END;
 
 IF COL_LENGTH('restaurant_tables', 'version') IS NULL
 BEGIN
     ALTER TABLE restaurant_tables ADD version BIGINT NULL;
-    UPDATE restaurant_tables SET version = 0 WHERE version IS NULL;
-    ALTER TABLE restaurant_tables ALTER COLUMN version BIGINT NOT NULL;
+    EXEC('UPDATE restaurant_tables SET version = 0 WHERE version IS NULL');
+    EXEC('ALTER TABLE restaurant_tables ALTER COLUMN version BIGINT NOT NULL');
 END;
+
+COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+    THROW;
+END CATCH;
